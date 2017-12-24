2169f65ba27d5e6f91e215bd36bd7f62bccc12fc
Alexis Robillart <alexisrobillart@gmail.com>
2017-12-24 01:27:31 +0100

    refactored code

--- script.js
+++ script.js
@@ -1,83 +1,81 @@
-const choices = document.querySelectorAll(`button[class="btn btn-outline-dark"]`);
+// Declare the variables needed.
 let playerRounds = 0;
 let computerRounds = 0;
 let tieRounds = 0;
-// Select and declate the html needed
-playerVersus = document.getElementById("player-versus");
-computerVersus = document.getElementById("computer-versus");
-versus = document.getElementById("versus");
-info = document.getElementById("info");
-containerInfo = document.getElementById("container-info");
+let playerVersus = document.getElementById("player-versus");
+let computerVersus = document.getElementById("computer-versus");
+let versus = document.getElementById("versus");
+let information = document.getElementById("information");
+let playerScore = document.getElementById("player-score");
+let computerScore = document.getElementById("computer-score");
+let tieScore = document.getElementById("tie-score");
 
+const choices = document.querySelectorAll(`button[class="btn btn-outline-dark"]`);
 choices.forEach(choice => choice.onclick = function() {
-  const playerSelection = choice.id;
-  const computerSelection = computerPlay();
+  let playerSelection = choice.id;
+  let computerSelection = computerPlay();
   playRound(playerSelection, computerSelection);
 });
 
 function computerPlay() {
-  let computerChoices = ["rock", "paper", "scissors"];
-  let random = Math.random();
-  random *= computerChoices.length;
-  random = Math.floor(random);
-  return(computerChoices[random])
-}
+  let possibleChoices = ["rock", "paper", "scissors"];
+  // Generate a random index number by taking a floating point number (between 0 and 1)
+  // then multiply it by the length the length of possibleChoices and convert it
+  // it to an integer.
+  return possibleChoices[Math.floor(possibleChoices.length * Math.random())];
+};
 
 function playRound(playerSelection, computerSelection) {
   playerVersus.classList.remove("rock", "paper", "scissors", "none");
   computerVersus.classList.remove("rock", "paper", "scissors", "none");
-  versus.classList.remove("win", "lose", "tie", "default")
-  // Add an image of the player selection with a class
+  versus.classList.remove("win", "lose", "tie", "default");
   playerVersus.classList.add(`${playerSelection}`);
   computerVersus.classList.add(`${computerSelection}`);
-  let beforeplayerRounds = playerRounds;
-  let beforecomputerRounds = computerRounds;
+  let beforePlayerRound = playerRounds;
+  let beforeComputerRound = computerRounds;
   if (playerSelection === "rock") {
     if (computerSelection === "paper") {
-      info.textContent = ("YOU LOSE!\r\nPaper beats Rock.");
-      computerRounds++
+      information.textContent = ("YOU LOSE!\r\nPaper beats Rock.");
+      computerRounds++;
     } else if (computerSelection === "scissors") {
-      info.textContent = ("YOU WIN!\r\nRock beats Scissors.");
-      playerRounds++
+      information.textContent = ("YOU WIN!\r\nRock beats Scissors.");
+      playerRounds++;
     } else {
-      info.textContent = ("IT'S A TIE!");
-      tieRounds++
+      information.textContent = ("IT'S A TIE!");
+      tieRounds++;
     }
   } else if (playerSelection === "paper") {
     if (computerSelection === "scissors") {
-      info.textContent = ("YOU LOSE!\r\nScissors beats Paper.");
-      computerRounds++
+      information.textContent = ("YOU LOSE!\r\nScissors beats Paper.");
+      computerRounds++;
     } else if (computerSelection === "rock") {
-      info.textContent = ("YOU WIN!\r\nPaper beats Rock.");
-      playerRounds++
+      information.textContent = ("YOU WIN!\r\nPaper beats Rock.");
+      playerRounds++;
     } else {
-      info.textContent = ("IT'S A TIE!");
-      tieRounds++
+      information.textContent = ("IT'S A TIE!");
+      tieRounds++;
     }
   } else {
     if (computerSelection === "rock") {
-      info.textContent = ("YOU LOSE!\r\nRock beats Scissors.");
-      computerRounds++
+      information.textContent = ("YOU LOSE!\r\nRock beats Scissors.");
+      computerRounds++;
     } else if (computerSelection === "paper") {
-      info.textContent = ("YOU WIN!\r\nScissors beats Paper.")
-      playerRounds++
+      information.textContent = ("YOU WIN!\r\nScissors beats Paper.");
+      playerRounds++;
     } else {
-      info.textContent = ("IT'S A TIE!");
-      tieRounds++
+      information.textContent = ("IT'S A TIE!");
+      tieRounds++;
     }
   }
-  // Checks the result of the round and apply the css accordodly.
-  if (playerRounds > beforeplayerRounds) {
+  // Checks who won the round.
+  if (playerRounds > beforePlayerRound) {
     versus.classList.add("win");
-  } else if (computerRounds > beforecomputerRounds) {
+  } else if (computerRounds > beforeComputerRound) {
     versus.classList.add("lose");
   } else {
     versus.classList.add("tie");
-  }
+  };
 
-  playerScore = document.getElementById("player-score");
-  computerScore = document.getElementById("computer-score");
-  tieScore = document.getElementById("tie-score");
   playerScore.textContent = playerRounds;
   computerScore.textContent = computerRounds;
   tieScore.textContent = tieRounds;
