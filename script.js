0632e2ee5d9f7002862ddfe5b997787a45974e11
Alexis Robillart <alexisrobillart@gmail.com>
2017-12-28 16:04:25 +0100

    add timeout to versus

--- script.js
+++ script.js
@@ -3,9 +3,6 @@ let playerVersus = document.getElementById("player-versus");
 let computerVersus = document.getElementById("computer-versus");
 let versus = document.getElementById("versus");
 let information = document.getElementById("information");
-let playerScore = document.getElementById("player-score");
-let computerScore = document.getElementById("computer-score");
-let tieScore = document.getElementById("tie-score");
 let playerSelection = null;
 let computerSelection = null;
 
@@ -60,9 +57,9 @@ function playRound(playerSelection, computerSelection) {
     }
   };
 
-  playerScore.textContent = score[0];
-  computerScore.textContent = score[1];
-  tieScore.textContent = score[2];
+  document.getElementById("player-score").textContent = score[0];
+  document.getElementById("computer-score").textContent = score[1];
+  document.getElementById("tie-score").textContent = score[2];
 
   if (score[0] === 5) {
     information.textContent = "YOU WIN THE GAME!";
@@ -78,21 +75,37 @@ function removeClass(item) {
 };
 
 function loseRound() {
+  setTimeout(function() {
+    versus.classList.remove("win");
+    versus.classList.add("default");
+  }, 1500);
   information.textContent = `YOU LOSE THE ROUND!\r\n${capitalizeFirstLetter(computerSelection)} beats ${capitalizeFirstLetter(playerSelection)}.`;
   score[1] += 1;
   versus.classList.add("lose");
+  setTimeout(function() {
+    versus.classList.remove("lose");
+    versus.classList.add("default");
+  }, 1400);
 };
 
 function winRound() {
   information.textContent = `YOU WIN THE ROUND!\r\n${capitalizeFirstLetter(playerSelection)} beats ${capitalizeFirstLetter(computerSelection)}.`;
   score[0] += 1;
   versus.classList.add("win");
+  setTimeout(function() {
+    versus.classList.remove("win");
+    versus.classList.add("default");
+  }, 1400);
 };
 
 function tieRound() {
   information.textContent = "IT'S A TIE ROUND!";
   score[2] += 1;
   versus.classList.add("tie");
+  setTimeout(function() {
+    versus.classList.remove("tie");
+    versus.classList.add("default");
+  }, 1400);
 };
 
 function capitalizeFirstLetter(string) {
